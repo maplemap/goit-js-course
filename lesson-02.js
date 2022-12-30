230b7be742446d4956dea06028d95b1ab4e4fbe3
Serhii Illarionov <sergio.illarionov@gmail.com>
2022-12-30 22:01:51 +0200

    add: tasks from lesson 2

--- lesson-02.js
+++ lesson-02.js
@@ -7,6 +7,16 @@
 // `ECMAScript`, то показуй alert з рядком `"Правильно!"`, в іншому випадку -
 // `"Не знаєте? ECMAScript!"`
 
+// const userInput = prompt('Яка офіційна назва JavaScript?');
+
+// if (userInput === null) {
+//   alert('Відхилено');
+// } else if (userInput === 'ECMAScript') {
+//   alert('Правильно!');
+// } else {
+//   alert('Не знаєте? ECMAScript!');
+// }
+
 // ## Example 2 - Відображення часу (if...else)
 
 // Напиши скрипт для відображення годин та хвилин у консолі браузера у вигляді рядка
@@ -15,7 +25,8 @@
 
 // const hours = 14;
 // const minutes = 26;
-// let timestring;
+// let timestring = minutes > 0 ? `${hours} г. ${minutes} хв.` : `${hours} г.`;
+// let timestring = `${hours} г.${minutes > 0 ? `${minutes} хв.` : ''}`;
 
 // if (minutes > 0) {
 //   timestring = `${hours} г. ${minutes} хв.`;
@@ -31,7 +42,16 @@
 // в консоль рядок `"Це нуль"`. Якщо передали від'ємне число, у консолі
 // повинен бути рядок `"Це негативне число"`.
 
-// const userInput = prompt('Введіть число');
+// let userInput = prompt('Введіть число');
+// userInput = Number(userInput);
+
+// if (userInput > 0) {
+//   console.log('Це позитивне число');
+// } else if (userInput === 0) {
+//   console.log('Це нуль');
+// } else {
+//   console.log('Це негативне число');
+// }
 
 // ## Example 4 - Вкладені розгалуження
 
@@ -40,7 +60,21 @@
 // випадку у консолі повинна бути сума значення `b` та числа 512.
 
 // const a = 120;
-// const b = 180;
+// const b = 110;
+
+// if (a > 100 && b > 100) {
+//   if (a > b) {
+//     console.log(a);
+//   } else {
+//     console.log(b);
+//   }
+//   const max = a > b ? a : b;
+//   console.log(max);
+
+//   console.log(Math.max(a, b));
+// } else {
+//   console.log(b + 512);
+// }
 
 // ## Example 5 - Форматування посилання (endsWith)
 
@@ -50,7 +84,11 @@
 
 // let link = 'https://my-site.com/about';
 // // Пиши код нижче за цей рядок
+// if (!link.endsWith('/')) {
+//   link = `${link}/`;
+// }
 
+// console.log(link);
 // // Пиши код вище за цей рядок
 // console.log(link);
 
@@ -61,9 +99,11 @@
 // тому випадку, якщо в `link` є підрядок `"my-site"`. Використовуй конструкцію
 // `if...else`.
 
-// let link = 'https://somesite.com/about';
-// // Пиши код нижче за цей рядок
-
+// let link = 'https://my-site.com/about';
+// // // Пиши код нижче за цей рядок
+// if (!link.endsWith('/') && link.includes('my-site')) {
+//   link = `${link}/`;
+// }
 // // Пиши код вище за цей рядок
 // console.log(link);
 
@@ -71,10 +111,13 @@
 
 // Виконай рефакторинг коду задачі номер 4, використовуючи тернарний оператор.
 
-// let link = 'https://somesite.com/about';
+// let link = 'https://my-site.com/about';
 // if (link.includes('my-site') && !link.endsWith('/')) {
 //   link += '/';
 // }
+
+// link += link.includes('my-site') && !link.endsWith('/') ? '/' : '';
+
 // console.log(link);
 
 // ## Example 8 - if...else та логічні оператори
@@ -88,7 +131,15 @@
 // - більше або дорівнює `17` і менше або дорівнює 24, виводь рядок `"Expires"`
 // - більше `24` , виводь рядок `"Overdue"`
 
-// const hours = 10;
+// const hours = 25;
+
+// if (hours < 17) {
+//   console.log('Pending');
+// } else if (hours >= 17 && hours <= 24) {
+//   console.log('Expires');
+// } else {
+//   console.log('Overdue');
+// }
 
 // ## Example 9 - Дедлайн здачі проекту (if...else)
 
@@ -100,14 +151,37 @@
 // - Якщо до дедлайну 2 дні - виведи рядок `"Післязавтра"`
 // - Якщо до дедлайну 3+ днів - виведи рядок `"Дата у майбутньому"`
 
-// const daysUntilDeadline = 5;
-// // Пиши код нижче за цей рядок
+// const daysUntilDeadline = 3;
+// // // Пиши код нижче за цей рядок
+// if (daysUntilDeadline === 0) {
+//   console.log('Сьогодні');
+// } else if (daysUntilDeadline === 1) {
+//   console.log('Завтра');
+// } else if (daysUntilDeadline === 2) {
+//   console.log('Післязавтра');
+// } else {
+//   console.log('Дата у майбутньому');
+// }
 
 // ## Example 10 - Дедлайн здачі проекту (switch)
 
 // Виконай рефакторинг коду задачі номер 5 використовуючи `switch`.
 
-// const daysUntilDeadline = 5;
+// const daysUntilDeadline = 4545;
+
+// switch (daysUntilDeadline) {
+//   case 0:
+//     console.log('Сьогодні');
+//     break;
+//   case 1:
+//     console.log('Завтра');
+//     break;
+//   case 2:
+//     console.log('Післязавтра');
+//     break;
+//   default:
+//     console.log('Дата у майбутньому');
+// }
 
 // if (daysUntilDeadline === 0) {
 //   console.log('Сьогодні');
@@ -127,6 +201,16 @@
 // const max = 100;
 // const min = 20;
 
+// for (let index = min; index <= max; index += 1) {
+//   if (index % 5 !== 0) {
+//     continue;
+//   }
+//   if (index % 5 === 0) {
+//     console.log(index);
+//   }
+//   console.log(index);
+// }
+
 // ## Example 12 - Введення користувача та розгалуження
 
 // Напиши скрипт, який питатиме логін за допомогою `prompt` та логувати
@@ -140,6 +224,17 @@
 
 // - Якщо введено пароль `"Я адмін"`, то вивести рядок `"Здрастуйте!"`
 // - Інакше виводити рядок `"Невірний пароль"`
+
+// const login = prompt('Give me your login dude!');
+
+// if (login === null) {
+//   console.log('Скасовано');
+// } else if (login === 'Адмін') {
+//   const password = prompt('Give me your password dude!');
+//   console.log(password === 'Я адмін' ? 'Здрастуйте' : 'Невірний пароль');
+// } else {
+//   console.log('Я вас не знаю');
+// }
 
 // ## Example 13 - Блоки кода. Вкладені області видимості
 // const level = 1;
@@ -160,3 +255,15 @@
 // f();
 
 // ## Example 15 - Оператор 'break'
+// for (let i = 0; i <= 5; i += 1) {
+//   console.log('loop 1', i);
+
+//   for (let i = 0; i <= 5; i += 1) {
+//     console.log('loop 2', i);
+
+//     if (i === 3) {
+//       console.log('Знайшли число 3, перериваємо виконання циклу');
+//       break;
+//     }
+//   }
+// }
