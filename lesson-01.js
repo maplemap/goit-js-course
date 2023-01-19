6a9d70e6284af7d58ad0554724e6f5b2ac2d42f7
Serhii Illarionov <sergio.illarionov@gmail.com>
2023-01-19 11:51:13 +0200

    add: solved tasks

--- lesson-01.js
+++ lesson-01.js
@@ -21,6 +21,37 @@
   }; 
 */
 
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+// console.log('перед змінами', user);
+
+//додає поле `mood` зі значенням `'happy'`
+// user.mood = 'happy';
+// const key = 'mood';
+// user[key] = 'happy';
+
+const newKey = 'own-phone';
+user[newKey] = 'dfsdfsdfs';
+
+//замінює значення `hobby` на `'skydiving'`
+// user.hobby = 'skydiving';
+user['hobby'] = 'skydiving';
+user.premium = false;
+
+// виводить вміст об'єкта `user` у форматі `ключ:значення` використовуючи
+// `Object.keys()` та `for...of`
+
+// const keys = Object.keys(user);
+// console.log(keys);
+
+// for (const key of keys) {
+//   console.log(`${key}: ${user[key]}`);
+// }
+
 /* 
   ## Example 2 - метод Object.values()
 
@@ -35,6 +66,27 @@
     Pete: 130,
   }; 
 */
+
+// const salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 100,
+// };
+// const keys = Object.keys(salaries);
+// let sum = 0;
+
+// for (const key of keys) {
+//   // console.log(salaries[key]);
+//   sum += salaries[key];
+// }
+
+// const values = Object.values(salaries);
+
+// for (const value of values) {
+//   sum += value;
+// }
+
+// console.log(sum);
 
 /* 
   ## Example 3 - Масив об'єктів
@@ -52,6 +104,28 @@
   ]; 
 */
 
+// const stones = [
+//   {name: 'Смарагд', price: 1300, quantity: 4},
+//   {name: 'Діамант', price: 2700, quantity: 3},
+//   {name: 'Сапфір', price: 400, quantity: 7},
+//   {name: 'Щебінь', price: 200, quantity: 2},
+// ];
+
+// const calcTotalPrice = function (stones, stoneName) {
+//   console.log('stoneName: ', stoneName);
+
+//   for (const stone of stones) {
+//     if (stoneName === stones.name) {
+//       console.log(stone);
+//       return stone.price * stone.quantity;
+//     }
+//   }
+
+//   return 0;
+// };
+
+// console.log(calcTotalPrice(stones, 'Смарагд'));
+
 /* 
   ## Example 4 - Комплексні завдання
 
@@ -64,13 +138,18 @@
  * Типів транзакцій всього два.
  * Можна покласти чи зняти гроші з рахунку.
  */
+const DEFAULT_NAME = 'name';
+const DEFAULT_AMOUNT = 100;
+const TIMEOUT = 3000;
+
 const TRANSACTION = {
-  DEPOSIT: 'deposit',
-  WITHDRAW: 'withdraw',
+  DEPOSIT: 'deposit', //покласти
+  WITHDRAW: 'withdraw', //зняти
 };
 
 /*
  * Кожна транзакція це об'єкт із властивостями: id, type та amount
+ * транзакція це {id: '', type: '', amount: ''}
  */
 
 const account = {
@@ -84,7 +163,13 @@ const account = {
    * Метод створює та повертає об'єкт транзакції.
    * Приймає суму та тип транзакції.
    */
-  createTransaction(amount, type) {},
+  createTransaction(amount, type) {
+    return {
+      type,
+      amount,
+      id: this.transactions.length,
+    };
+  },
 
   /*
    * Метод, що відповідає за додавання суми до балансу.
@@ -92,7 +177,12 @@ const account = {
    * Викликає createTransaction для створення об'єкта транзакції
    * після чого додає його до історії транзакцій
    */
-  deposit(amount) {},
+  deposit(amount) {
+    this.balance += amount;
+
+    const transaction = this.createTransaction(amount, TRANSACTION.DEPOSIT);
+    this.transactions.push(transaction);
+  },
 
   /*
    * Метод, що відповідає за зняття суми з балансу.
@@ -103,12 +193,26 @@ const account = {
    * Якщо amount більше ніж поточний баланс, виводь повідомлення
    * про те, що зняття такої суми не можливе, недостатньо коштів.
    */
-  withdraw(amount) {},
+  withdraw(amount) {
+    if (amount <= this.balance) {
+      this.balance -= amount;
+      const transaction = this.createTransaction(amount, TRANSACTION.WITHDRAW);
+      this.transactions.push(transaction);
+    } else {
+      console.error('Недостатньо коштів на рахунку');
+    }
+  },
 
   /*
    * Метод повертає поточний баланс
    */
-  getBalance() {},
+  getBalance() {
+    return this.balance;
+  },
+
+  getTransactions() {
+    return this.transactions;
+  },
 
   /*
    * Метод шукає та повертає об'єкт транзакції по id
@@ -121,3 +225,45 @@ const account = {
    */
   getTransactionTotal(type) {},
 };
+
+console.log('getBalance', account.getBalance());
+
+account.deposit(120);
+console.log('getBalance', account.getBalance());
+console.log('getTransactions', account.getTransactions());
+
+account.withdraw(80);
+console.log('getTransactions', account.getTransactions());
+console.log('getBalance', account.getBalance());
+
+// const products = [
+//   {name: 'Radar', price: 1300, quantity: 4},
+//   {name: 'Scanner', price: 2700, quantity: 3},
+//   {name: 'Droid', price: 400, quantity: 7},
+//   {name: 'Grip', price: 1200, quantity: 9},
+// ];
+
+// Напиши функцію calculateTotalPrice(productName),
+// яка приймає один параметр productName - назва товару.
+// Функція повинна повернути загальну вартість (ціна * кількість) товару з таким ім'ям з масиву products.
+
+// function calculateTotalPrice(productName) {
+//   console.log('productName: ', productName);
+//   // знайти
+//   // перемножити price на quantity
+//   // повернути результат
+
+//   for (const product of products) {
+//     if (productName === product.name) {
+//       return product.price * product.quantity;
+//     }
+//   }
+
+//   return 0;
+// }
+
+// console.log(calculateTotalPrice('Blaster')); // повертає 0
+// console.log(calculateTotalPrice('Radar')); // повертає 5200
+// console.log(calculateTotalPrice('Droid')); // повертає 2800
+// console.log(calculateTotalPrice('Grip')); // повертає 10800
+// console.log(calculateTotalPrice('Scanner')); // повертає 8100
