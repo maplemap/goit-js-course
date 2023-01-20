6eb78f45a8c08cadbd3a4dd5de8bbaf9a84d660c
Serhii Illarionov <sergio.illarionov@gmail.com>
2023-01-20 22:02:04 +0200

    add: solved tasks

--- lesson-02.js
+++ lesson-02.js
@@ -39,6 +39,32 @@
     ); 
 */
 
+// function calcBMI({weight, height}) {
+//   const numericWeight = Number(weight.replace(',', '.'));
+//   const numericHeight = Number(height.replace(',', '.'));
+//   return Number((numericWeight / numericHeight ** 2).toFixed(1));
+// }
+
+// console.log(
+//   calcBMI({
+//     weight: '88,3',
+//     height: '1.75',
+//   }),
+// );
+
+// console.log(
+//   calcBMI({
+//     weight: '68,3',
+//     height: '1.65',
+//   }),
+// );
+// console.log(
+//   calcBMI({
+//     weight: '118,3',
+//     height: '1.95',
+//   }),
+// );
+
 /* 
     ## Example 2 - Деструктуризація
 
@@ -67,6 +93,19 @@
     });
 */
 
+// function printContactsInfo({names, phones}) {
+//   const nameList = names.split(',');
+//   const phoneList = phones.split(',');
+//   for (let i = 0; i < nameList.length; i += 1) {
+//     console.log(`${nameList[i]}: ${phoneList[i]}`);
+//   }
+// }
+
+// printContactsInfo({
+//   phones: '89001234567,89001112233,890055566377,890055566300',
+//   names: 'Jacob,William,Solomon,Artemis',
+// });
+
 /* 
     ## Example 3 - Глибока деструктуризація
 
@@ -92,6 +131,33 @@
     }),
     ); // "Cyberdyne Systems has 200 bots in stock"
 */
+
+// function getBotReport({companyName, bots: {repair, defence}}) {
+//   return `${companyName} has ${repair + defence} bots in stock`;
+// }
+
+// console.log(
+//   getBotReport({
+//     companyName: 'Cyberdyne Systems',
+//     bots: {
+//       repair: 150,
+//       defence: 50,
+//     },
+//   }),
+// );
+
+// const {
+//   companyName,
+//   bots: {repair, defence},
+// } = {
+//   companyName: 'Cyberdyne Systems',
+//   bots: {
+//     repair: 150,
+//     defence: 50,
+//   },
+// };
+
+// console.log(companyName, repair, defence);
 
 /* 
     ## Example 4 - Деструктуризація
@@ -125,6 +191,38 @@
     ); // "Belacci has 35 item in stock"
 */
 
+// function getStockReport({companyName, stock}) {
+//   const goodsPrices = Object.values(stock);
+//   let total = 0;
+
+//   for (const goodsPrice of goodsPrices) {
+//     total += goodsPrice;
+//   }
+
+//   return `${companyName} has ${total} items in stock`;
+// }
+
+// console.log(
+//   getStockReport({
+//     companyName: 'Cyberdyne Systems',
+//     stock: {
+//       repairBots: 150,
+//       defenceBots: 50,
+//     },
+//   }),
+// ); // "Cyberdyne Systems has 200 items in stock"
+
+// console.log(
+//   getStockReport({
+//     companyName: 'Belacci',
+//     stock: {
+//       shoes: 20,
+//       skirts: 10,
+//       hats: 5,
+//     },
+//   }),
+// ); // "Belacci has 35 item in stock"
+
 /* 
     ## Example 5 - Операція spread
 
@@ -154,6 +252,40 @@
     }),
     );
 */
+
+// function generateId() {
+//   return '_' + Math.random().toString(36).slice(2);
+// }
+
+// function createContact(partialContact) {
+//   //   const defaultContactData = {
+//   //     id: generateId(),
+//   //     createdAt: Date.now(),
+//   //     list: 'default',
+//   //   };
+
+//   return {
+//     // ...defaultContactData,
+//     id: generateId(),
+//     createdAt: Date.now(),
+//     list: 'default',
+//     ...partialContact,
+//   };
+// }
+
+// console.log(
+//   createContact({
+//     name: 'Mango',
+//     email: 'mango@mail.com',
+//     list: 'friends',
+//   }),
+// );
+// console.log(
+//   createContact({
+//     name: 'Poly',
+//     email: 'poly@hotmail.com',
+//   }),
+// );
 
 /* 
     ## Example 6 - Операція rest
@@ -186,3 +318,32 @@
     }),
     );
 */
+
+function transformUsername({firstName, lastName, ...restParams}) {
+  return {
+    ...restParams,
+    fullName: `${firstName} ${lastName}`,
+  };
+}
+
+// console.log(
+//   transformUsername({
+//     id: 1,
+//     firstName: 'Jacob',
+//     lastName: 'Mercer',
+//     email: 'j.mercer@mail.com',
+//     friendCount: 40,
+//     friends: ['sdfsdf', 'sdfsdf', 'sdasdasd'],
+//     address: 'Address',
+//   }),
+// );
+
+// console.log(
+//   transformUsername({
+//     id: 2,
+//     firstName: 'Adrian',
+//     lastName: 'Cross',
+//     email: 'a.cross@hotmail.com',
+//     friendCount: 20,
+//   }),
+// );
