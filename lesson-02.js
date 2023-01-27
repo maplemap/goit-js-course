f2c68b16144187564ebf5d225314f25ec0a6fd83
Serhii Illarionov <sergio.illarionov@gmail.com>
2023-01-27 22:22:04 +0200

    add: solved tasks

--- lesson-02.js
+++ lesson-02.js
@@ -95,6 +95,37 @@ const cars = [
     console.table(getModels(cars));
 */
 
+//v1
+// const getModels = (cars) => {
+//   const result = cars.map((car) => {
+//     return car.model;
+//   });
+//   return result;
+// };
+
+//v2
+// const getModels = (cars) => cars.map(({model}) => model);
+
+// console.table(getModels(cars));
+/* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 /*
     ## Example 2 - Метод map
 
@@ -107,18 +138,100 @@ const cars = [
     console.table(makeCarsWithDiscount(cars, 0.4));
 */
 
+// price * 0.2 - знижка
+// price - (знижка)
+
+//v1
+// const makeCarsWithDiscount = (cars, discount) => {
+//   const result = cars.map((car) => {
+//     const discountPrice = car.price - car.price * discount;
+
+//     return {
+//       ...car,
+//       price: discountPrice,
+//     };
+//   });
+
+//   return result;
+// };
+
+//v2
+// const makeCarsWithDiscount = (cars, discount) =>
+//   cars.map((car) => ({
+//     ...car,
+//     price: car.price - car.price * discount,
+//   }));
+
+// console.table(makeCarsWithDiscount(cars, 0.2));
+// console.table(makeCarsWithDiscount(cars, 0.4));
+
+/* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 /*
     ## Example 3 - Метод filter
 
-    Нехай функція `filterByPrice` повертає масив автомобілів ціна яких менша
-    ніж значення параметра `threshold`.
+    Нехай функція `filterByPrice` повертає масив автомобілів ціна 
+    яких менша ніж значення параметра `threshold`.
 
     const filterByPrice = (cars, threshold) => {};
 
     console.table(filterByPrice(cars, 30000));
     console.table(filterByPrice(cars, 25000));
 */
+//v1
+// const filterByPrice = (cars, threshold) => {
+//   const result = cars.filter((car) => {
+//     return car.price < threshold;
+//   });
 
+//   return result;
+// };
+
+// v2
+// const filterByPrice = (cars, threshold) =>
+//   cars.filter(({price}) => price < threshold);
+
+// console.table(filterByPrice(cars, 30000));
+// console.table(filterByPrice(cars, 25000));
+
+/* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 /*
     ## Example 4 - Метод filter
 
@@ -130,6 +243,40 @@ const cars = [
     console.table(getCarsWithDiscount(cars));
 */
 
+//v1
+// const getCarsWithDiscount = (cars) => {
+//   const result = cars.filter((car) => {
+//     // return car.onSale;
+//   });
+
+//   return result;
+// };
+
+//v2
+// const getCarsWithDiscount = (cars) => cars.filter(({onSale}) => onSale);
+
+// console.table(getCarsWithDiscount(cars));
+
+/* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 /*
     ## Example 5 - Метод filter
 
@@ -142,6 +289,26 @@ const cars = [
     console.table(getCarsWithType(cars, 'sedan'));
 */
 
+/* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 /*
     ## Example 6 - Метод find
 
@@ -151,12 +318,46 @@ const cars = [
     console.log(getCarByModel(cars, 'F-150'));
     console.log(getCarByModel(cars, 'CX-9'));
 */
+//v1
+// const getCarByModel = (cars, model) => {
+//   const result = cars.find((car) => {
+//     return car.model === model;
+//   });
 
+//   return result;
+// };
+
+//v2
+// const getCarByModel = (cars, model) => cars.find((car) => car.model === model);
+
+// console.log(getCarByModel(cars, 'F-150'));
+// console.log(getCarByModel(cars, 'CX-9'));
+
+/* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 /*
     ## Example 7 - Метод sort
 
-    Нехай функція `sortByAscendingAmount` повертає новий масив автомобілів відсортований за 
-    зростанням значення якості `amount`.
+    Нехай функція `sortByAscendingAmount` повертає новий масив 
+    автомобілів відсортований за зростанням значення ключу `amount`.
 
 
     const sortByAscendingAmount = cars => {};
@@ -164,6 +365,36 @@ const cars = [
     console.table(sortByAscendingAmount(cars));
 */
 
+// const sortByAscendingAmount = (cars) => {
+//   const result = cars.sort((a, b) => {
+//     return a.amount - b.amount;
+//   });
+
+//   console.table(result);
+// };
+
+// console.table(sortByAscendingAmount(cars));
+
+/* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 /*
     ## Example 8 - Метод sort
 
@@ -176,6 +407,26 @@ const cars = [
     console.table(sortByDescendingPrice(cars));
 */
 
+/* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 /*
     ## Example 9 - Метод sort
 
@@ -189,7 +440,26 @@ const cars = [
     console.table(sortByModel(cars, 'asc'));
     console.table(sortByModel(cars, 'desc'));
 */
+/* 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 /*
     ## Example 10 - Метод reduce
 
@@ -202,6 +472,93 @@ const cars = [
     console.log(getTotalAmount(cars));
 */
 
+//v1
+// const getTotalAmount = (cars) => {
+//   const result = cars.reduce((acc, car) => {
+//     acc += car.amount;
+//     return acc;
+//   }, 0);
+
+//   return result;
+// };
+
+//v2 // все те що повертається з коллбеку - записується в acc
+// const getTotalAmount = (cars) => cars.reduce((acc, car) => acc + car.amount, 0);
+
+// console.log(getTotalAmount(cars));
+
+//calсulateAverage
+// const calсulateAverage = (...args) => {
+//   let total = 0;
+
+//   args.forEach((candiBober) => {
+//     total += candiBober;
+//   });
+
+//   return total / args.length;
+// };
+
+//v1
+// const calсulateAverage = (...args) => {
+//   // let total = 0;
+
+//   // args.forEach((candiBober) => {
+//   //   total += candiBober;
+//   // });
+
+//   const total = args.reduce((acc, number) => {
+//     acc += number;
+//     return acc;
+//   }, 0);
+
+//   return total / args.length;
+// };
+
+//v2
+// const calсulateAverage = (...args) =>
+//   args.reduce((acc, number) => acc + number, 0) / args.length;
+
+// console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
+
+/* reduce який повертає об'єкт */
+
+// const result = cars.reduce(
+//   (acc, car) => {
+//     acc.totalAmount += car.amount;
+//     acc.totalPrice += car.price;
+//     return acc;
+//   },
+//   {totalAmount: 0, totalPrice: 0},
+// ); //{totalAmount, totalPrice}
+
+// console.log(result);
+
+/* reduce який повертає строку */
+const result = cars.reduce((acc, car) => {
+  acc += `_${car.model}`;
+
+  return acc;
+}, ''); //{totalAmount, totalPrice}
+
+console.log(result);
+/* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 /* 
     ## Example 11 - Ланцюжки методів
 
@@ -213,7 +570,26 @@ const cars = [
 
     console.table(getModelsOnSale(cars));
 */
+/* 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 /*
     ## Example 12 - Ланцюжки методів
 
