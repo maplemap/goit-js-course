4cf63e731af9c4b94de485050f134c481e81e4b1
Serhii Illarionov <sergio.illarionov@gmail.com>
2023-02-13 14:45:08 +0200

    create: materials ro module 06

---
+++ js/color-buttons.js
@@ -0,0 +1,28 @@
+/* 
+    - Об'єкти window і document
+    - Ієрархія DOM-елементів
+    - Пошук DOM-елементів із querySelector
+    - Властивість textContent
+    - Створення елементів
+    - Видалення методом remove
+
+    Створити кілька кнопок на основі масива з обєктами використовуючи createElement
+*/
+const colors = [
+  {
+    label: 'red',
+    color: '#FF0000',
+  },
+  {
+    label: 'green',
+    color: '#00FF00',
+  },
+  {
+    label: 'blue',
+    color: '#0000FF',
+  },
+  {
+    label: 'yellow',
+    color: '#FFFF00',
+  },
+];
---
+++ js/list-of-technologies.js
@@ -0,0 +1,12 @@
+/* 
+    - Об'єкти window і document
+    - Ієрархія DOM-елементів
+    - Пошук DOM-елементів із querySelector
+    - Властивість textContent
+    - Створення елементів
+
+    Створити список технологій що вивчаються на курсі 
+    Fullstack за допомогою js (map, reduce)
+*/
+
+const technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
