8999b20f2da2a89f94ea3ec48883a8faf5500270
Serhii Illarionov <sergio.illarionov@gmail.com>
2023-02-17 22:07:26 +0200

    add: solved tasks

--- js/login-form.js
+++ js/login-form.js
@@ -11,7 +11,35 @@
         3. Захаркодити вірні логін і пароль в константах
         4. Додати логіку сабміта форми, при сабміті перевіряємо введені логін і пароль.
             1. Якщо логін і пароль співпадають - видаляємо форму з документа і 
-            показуємо **h2**  з написом **“Вхід успішний”** - h2>Вхід успішний</h2>
+            показуємо **h2**  з написом **“Вхід успішний”** - <h2>Вхід успішний</h2>
             2. Якщо логін і пароль не співпадають з даними з констант в пункті 3с - 
             показуємо під формою помилку червоним кольором **“Логін або пароль не вірні”**
 */
+
+const EMAIL = 'admin@mail.ua';
+const PASSWORD = '111';
+
+const formRef = document.querySelector('.form');
+const errorsRef = formRef.querySelector('.form__errors');
+const loginInputRef = formRef.querySelector('input[type="email"]');
+const passwordInputRef = formRef.querySelector('input[type="password"]');
+
+loginInputRef.addEventListener('focus', hideErrors);
+passwordInputRef.addEventListener('focus', hideErrors);
+
+formRef.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const {email, password} = e.currentTarget.elements;
+  console.log(e.currentTarget.elements);
+
+  if (email.value === EMAIL && password.value === PASSWORD) {
+    formRef.remove();
+    document.body.insertAdjacentHTML('beforeend', '<h2>Вхід успішний</h2>');
+  } else {
+    errorsRef.classList.add('form__errors_shown');
+  }
+});
+
+function hideErrors() {
+  errorsRef.classList.remove('form__errors_shown');
+}
--- js/modal-window.js
+++ js/modal-window.js
@@ -1,6 +1,5 @@
 /* 
     - Видалення методом remove
-    - Властивість innerHTML і метод insertAdjacentHTML
     - Події click, keydown
 
 
@@ -8,3 +7,32 @@
     кнопку всередині тега body. Модальне вікно має закриватися при 
     кліку на оверлей, та на іконку хрестика всередині модалки
 */
+
+const ESCAPE_BTN_CODE = 'Escape';
+
+const buttonRef = document.querySelector('.btn.trigger');
+const closeButtonRef = document.querySelector('.btn-close.trigger');
+const modalWrapperRef = document.querySelector('.modal-wrapper');
+
+buttonRef.addEventListener('click', openModal);
+closeButtonRef.addEventListener('click', closeModal);
+modalWrapperRef.addEventListener('click', (e) => {
+  if (e.currentTarget === e.target) {
+    closeModal();
+  }
+});
+
+function openModal() {
+  modalWrapperRef.classList.add('open');
+  document.addEventListener('keydown', closeModalByEscape);
+}
+function closeModal() {
+  modalWrapperRef.classList.remove('open');
+  document.removeEventListener('keydown', closeModalByEscape);
+}
+
+function closeModalByEscape(e) {
+  if (e.code === ESCAPE_BTN_CODE) {
+    closeModal();
+  }
+}
