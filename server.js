2d5ebd0a8dcd92e0e03263124f409e0a9ab17937
Serhii Illarionov <sergio.illarionov@gmail.com>
2023-03-24 16:36:51 +0200

    add: server

---
+++ server.js
@@ -0,0 +1,37 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// Add custom routes before JSON Server router
+server.get('/echo/:id', (req, res) => {
+  const timeout = getRandomArbitrary(300, 1500);
+
+  setTimeout(() => {
+    res.send({ id: req.params.id });
+  }, timeout);
+});
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+// Use default router
+server.use(router);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
--- src/modules/services/tasks.js
+++ src/modules/services/tasks.js
@@ -1,13 +1,8 @@
 import axios from 'axios';
-/* 
-  C-create R-read U-update D-delete
-*/
 
 const BASE_URL = 'http://localhost:3000';
 
-class TasksServiceWithFetch {
-  // R-read
-  // GET
+class TasksService {
   getTasks() {
     return fetch(`${BASE_URL}/tasks`).then((response) => response.json());
   }
@@ -16,8 +11,6 @@ class TasksServiceWithFetch {
     return fetch(`${BASE_URL}/tasks/${id}`).then((response) => response.json());
   }
 
-  // C-create
-  // POST
   createTask(task) {
     const options = {
       method: 'POST',
@@ -30,8 +23,6 @@ class TasksServiceWithFetch {
     return fetch(`${BASE_URL}/tasks`, options).then((response) => response.json());
   }
 
-  // U-Update
-  // PATCH
   updateTask(id, data) {
     const options = {
       method: 'PATCH',
@@ -44,53 +35,13 @@ class TasksServiceWithFetch {
     return fetch(`${BASE_URL}/tasks/${id}`, options).then((response) => response.json());
   }
 
-  // D-Delete
-  // DELETE
   deleteTask(id) {
-    // const options = {
-    //   method: 'DELETE',
-    // };
+    const options = {
+      method: 'DELETE',
+    };
 
-    // return fetch(`${BASE_URL}/tasks/${id}`, options).then((response) => response.json());
-    return axios.delete(`${BASE_URL}/tasks/${id}`);
-  }
-}
-
-class TasksService {
-  getTasks() {
-    return axios.get(`${BASE_URL}/tasks`).then(({ data }) => data);
-  }
-
-  getTaskById(id) {
-    return axios.get(`${BASE_URL}/tasks/${id}`).then(({ data }) => data);
-  }
-
-  createTask(newTask) {
-    return axios.post(`${BASE_URL}/tasks`, newTask).then(({ data }) => data);
-  }
-
-  updateTask(id, data) {
-    return axios.patch(`${BASE_URL}/tasks/${id}`, data).then(({ data }) => data);
-  }
-
-  deleteTask(id) {
-    return axios.delete(`${BASE_URL}/tasks/${id}`);
+    return fetch(`${BASE_URL}/tasks/${id}`, options).then((response) => response.json());
   }
 }
 
 export default new TasksService();
-
-// const taskService = new TasksService();
-
-// taskService.getTasks().then((data) => console.log(data));
-// taskService.getTaskById(13).then((data) => console.log(data));
-// taskService.createTask({ value: 'Манго', done: false }).then((data) => console.log(data));
-// taskService
-//   .updateTask(356, { value: 'Tomato Green' })
-//   .then((data) => {
-//     console.log('then', data);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-// taskService.deleteTask(11).then((data) => console.log(data));
